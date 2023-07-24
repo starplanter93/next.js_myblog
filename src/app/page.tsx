a4f65ef30b8d88968fec1ea526e96edb69fd4f17
starplanter93 <starplanter93@gmail.com>
2023-07-24 17:42:52 +0900

    메인페이지 Featured Posts에 더미데이터 받아오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Myprofile from '../components/home/Myprofile';
+import Featuredposts from '@/components/home/Featuredposts';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Myprofile />
+      <Featuredposts />
     </main>
   );
 }
---
+++ src/components/home/Featuredposts.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import posts from '../../../public/data/posts.json';
+import Link from 'next/link';
+import Image from 'next/image';
+
+function Featuredposts() {
+  const featuredPosts = posts.filter((post) => post.featured);
+  return (
+    <>
+      <h2>Featured Posts</h2>
+      <ul>
+        {featuredPosts.map(
+          ({ title, description, category, date, path }, index) => (
+            <Link href={`posts/${path}`} key={index}>
+              <Image
+                src={`/images/posts/${path}.png`}
+                alt='post-image'
+                width={250}
+                height={250}
+              />
+              <div>{date}</div>
+              <div>{title}</div>
+              <div>{description}</div>
+              <div>{category}</div>
+            </Link>
+          )
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default Featuredposts;
