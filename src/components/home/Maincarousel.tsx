cd4817243efa0d8e1f2703a871383842e089a595
starplanter93 <starplanter93@gmail.com>
2023-08-02 17:25:49 +0900

    메인 캐러셀 세부 설정 변경

--- src/components/home/Maincarousel.tsx
+++ src/components/home/Maincarousel.tsx
@@ -17,12 +17,13 @@ function MainCarousel() {
       items: 5,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      breakpoint: { max: 3000, min: 800 },
+      items: 4,
+      // partialVisibilityGutter: 100,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      breakpoint: { max: 800, min: 464 },
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -30,34 +31,49 @@ function MainCarousel() {
     },
   };
   return (
-    <Carousel className='w-full' responsive={responsive} ssr={true}>
-      {unfeaturedPosts.map(
-        ({ title, description, category, date, path }, index) => (
-          <li
-            className='border border-solid border-inherit rounded-md shadow-md hover:shadow-lg'
-            key={index}
-          >
-            <Link className='flex flex-col items-center' href={`posts/${path}`}>
-              <Image
-                className='w-full rounded-t-md'
-                src={`/images/posts/${path}.png`}
-                alt='post-image'
-                width={500}
-                height={250}
-              />
-              <div className='mt-4 pr-3 self-end'>{date}</div>
-              <div className={`my-2 ${gothic800.className} text-md`}>
-                {title}
-              </div>
-              <div>{description}</div>
-              <div className='mt-2 mb-4 px-1 border border-transparent rounded-md bg-emerald-100'>
-                {category}
-              </div>
-            </Link>
-          </li>
-        )
-      )}
-    </Carousel>
+    <div className='w-full mb-8'>
+      <h2 className={`${gothic800.className} text-lg my-4`}>You May Like</h2>
+      <Carousel
+        className='w-full'
+        // partialVisible={true}
+        responsive={responsive}
+        infinite
+        arrows
+        centerMode={true}
+        autoPlay
+        autoPlaySpeed={3000}
+      >
+        {unfeaturedPosts.map(
+          ({ title, description, category, date, path }, index) => (
+            <li
+              className='w-11/12 border border-solid border-inherit rounded-md shadow-md hover:shadow-lg'
+              key={index}
+            >
+              <Link
+                className='flex flex-col items-center'
+                href={`posts/${path}`}
+              >
+                <Image
+                  className='w-full rounded-t-md'
+                  src={`/images/posts/${path}.png`}
+                  alt='post-image'
+                  width={400}
+                  height={400}
+                />
+                <div className='mt-4 pr-3 self-end'>{date}</div>
+                <div className={`my-2 ${gothic800.className} text-md`}>
+                  {title}
+                </div>
+                <div>{description}</div>
+                <div className='mt-2 mb-4 px-1 border border-transparent rounded-md bg-emerald-100'>
+                  {category}
+                </div>
+              </Link>
+            </li>
+          )
+        )}
+      </Carousel>
+    </div>
   );
 }
 
