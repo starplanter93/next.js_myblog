78e88cf1b274f9a5d9689103ca5820b5c281de91
starplanter93 <starplanter93@gmail.com>
2023-09-10 17:26:58 +0900

    :lipstick: Postaside 선택된 카테고리에 색 추가
    카테고리를 클릭하면 색이 바뀝니다

--- src/components/posts/Postaside.tsx
+++ src/components/posts/Postaside.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import posts from '../../../data/posts.json';
 
 interface Ownprops {
@@ -7,27 +7,42 @@ interface Ownprops {
 }
 
 function Postaside({ categoryHandler }: Ownprops) {
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
   const categories = posts.map((post) => post.category);
   const set = new Set(categories);
   const uniqueCategories = [...set];
 
+  const onClickCategory = (e: React.MouseEvent) => {
+    categoryHandler(e.currentTarget.innerHTML);
+    setSelectedCategory((prev) => {
+      if (prev === e.currentTarget.innerHTML) {
+        return '';
+      }
+      e.currentTarget.innerHTML;
+      return e.currentTarget.innerHTML;
+    });
+  };
+
   return (
-    <aside>
-      <div>Category</div>
+    <aside className='flex flex-col mt-3 ml-6'>
+      <div className='mb-3 font-bold border-sky-500 border-b-4'>Category</div>
       <ul>
         <li
+          className={`flex justify-center`}
           onClick={(e) => {
             categoryHandler(e.currentTarget.innerHTML);
+            setSelectedCategory(e.currentTarget.innerHTML);
           }}
         >
           All Posts
         </li>
         {uniqueCategories.map((category, index) => (
           <li
+            className={`flex justify-center ${
+              selectedCategory === category ? 'text-sky-500' : 'text-black'
+            }`}
             key={index}
-            onClick={(e) => {
-              categoryHandler(e.currentTarget.innerHTML);
-            }}
+            onClick={onClickCategory}
           >
             {category}
           </li>
